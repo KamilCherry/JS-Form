a60a955bf11131eac628d7adc1e82bd5d49274e0
Cherry <wisnikamil@protonmail.com>
2022-11-19 23:03:46 +0100

    Add check for date and the purpose above the input tag

--- data/js/validate.js
+++ data/js/validate.js
@@ -1,21 +1,23 @@
 
 const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const regexPassword = /(?=.*\d).{8,}/;
+const regexDate = /^((20)|(19))(\d){2}-(((\d){2})|(10-12))-(\d){2}/;
 const notTheThing = null;
 
 function start() {
     document.getElementsByClassName("errorInfo")[0].style.display = "none";
     document.getElementsByClassName("errorInfo")[0].getElementsByTagName("ul")[0].innerHTML = "";
-    document.getElementById("submit").addEventListener("click", function() {validate(document.getElementById("email"), document.getElementById("passwd"), document.getElementById("passwdAgain"), false)});
+    document.getElementById("submit").addEventListener("click", function() {validate(document.getElementById("email"), document.getElementById("passwd"), document.getElementById("passwdAgain"), document.getElementById("dateOfBirth"), notTheThing, false)});
 
-    document.getElementById("email").addEventListener("blur", function() {validate(document.getElementById("email"), notTheThing, notTheThing, true)});
-    document.getElementById("passwd").addEventListener("blur", function() {validate(notTheThing, document.getElementById("passwd"), notTheThing, true)});
-    document.getElementById("passwdAgain").addEventListener("blur", function() {validate(notTheThing, document.getElementById("passwd"), document.getElementById("passwdAgain"), true)});
+    document.getElementById("dateOfBirth").addEventListener("blur", function() {validate(notTheThing, notTheThing, notTheThing, document.getElementById("dateOfBirth"), true)});
+    document.getElementById("email").addEventListener("blur", function() {validate(document.getElementById("email"), notTheThing, notTheThing, notTheThing, true)});
+    document.getElementById("passwd").addEventListener("blur", function() {validate(notTheThing, document.getElementById("passwd"), notTheThing, notTheThing, true)});
+    document.getElementById("passwdAgain").addEventListener("blur", function() {validate(notTheThing, document.getElementById("passwd"), document.getElementById("passwdAgain"), notTheThing, true)});
 }
 
 window.onload = start;
 
-function validate(email, password, passwordSecond, blur) {
+function validate(email, password, passwordSecond, doB, blur) {
 
     if (!blur) {
         document.getElementsByClassName("errorInfo")[0].style.display = "none";
@@ -25,6 +27,7 @@ function validate(email, password, passwordSecond, blur) {
     if (email) email.classList.remove("redBorder");
     if (password) password.classList.remove("redBorder");
     if (passwordSecond) passwordSecond.classList.remove("redBorder");
+    if (doB) doB.classList.remove("redBorder");
 
     let everythingOK = true;
 
@@ -43,6 +46,12 @@ function validate(email, password, passwordSecond, blur) {
     if (password && passwordSecond)
     if (!(passwordSecond.value == password.value)) {
         displayIncorrect("Password and second password aren't the same.", passwordSecond, blur);
+        everythingOK = false;
+    }
+
+    if (doB)
+    if (!doB.value.match(regexDate)) {
+        displayIncorrect("Incorrect date. Proper format DD/MM/YYYY", doB, blur);
         everythingOK = false;
     }
 
